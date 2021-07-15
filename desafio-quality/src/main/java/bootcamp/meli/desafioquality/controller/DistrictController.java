cee1df7a097198a59fe134c0766e1aac98776708
iBianquini <iago.bianquini@mercadolivre.com>
2021-07-15 16:08:40 -0300

    Update desafio-quality/src/main/java/bootcamp/meli/desafioquality/controller/DistrictController.java
    Co-authored-by: Daniel Silva Nunes <daniel.nunes@mercadolivre.com>

--- desafio-quality/src/main/java/bootcamp/meli/desafioquality/controller/DistrictController.java
+++ desafio-quality/src/main/java/bootcamp/meli/desafioquality/controller/DistrictController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/quality/district")
+@RequestMapping("/quality")
 public class DistrictController {
 
     @Autowired
